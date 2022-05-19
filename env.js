9fdd13d5ab03c5143625fcea1d0c44a8799bdc3f
samirbenbouker <57891254+samirbenbouker@users.noreply.github.com>
2022-05-19 15:35:52 +0200

    Delete env.js

--- env.js
+++
@@ -1,6 +0,0 @@
-MONGO_URL = "mongodb+srv://samirbenbouker:DgeWXxtPjTwfozLs@cluster0.wnl0p.mongodb.net/shop?retryWrites=true&w=majority"
-PORT = 5000
-JWT_SEC = "samirbenbouker"
-PASS_ENC = "samirbenbouker"
-NODE_ENV = "production"
-module.exports = {MONGO_URL, PORT, JWT_SEC, PASS_ENC, NODE_ENV}
